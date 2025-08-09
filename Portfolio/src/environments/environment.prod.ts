2c2f26d6fec4aaaa6c9ed40e2741a33490c2b9cb
E7med <111331925+E7med@users.noreply.github.com>
2025-08-09 14:28:07 +0300

    Add files via upload

---
+++ Portfolio/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  emailServiceID: 'service_7ot937a',
+  emailTemplateID: 'template_d6ka8lj',
+  emailPublicKey: 'MGLdl118S5BxuOgdQ'
+};
---
+++ Portfolio/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  emailServiceID: 'service_7ot937a',
+  emailTemplateID: 'template_d6ka8lj', 
+  emailPublicKey: 'MGLdl118S5BxuOgdQ'
+};
